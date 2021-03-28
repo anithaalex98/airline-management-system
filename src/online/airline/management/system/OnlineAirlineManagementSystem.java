cafef12e96b15c7b9f7fa2ccd6219e66e69f8f03
Anitha Susan Alex <anithaalex98@gmail.com>
2021-03-29 00:17:44 +0530

    airline project

---
+++ src/online/airline/management/system/OnlineAirlineManagementSystem.java
@@ -0,0 +1,22 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package online.airline.management.system;
+
+/**
+ *
+ * @author Pradeep John
+ */
+public class OnlineAirlineManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
